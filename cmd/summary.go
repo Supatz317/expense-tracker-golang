bdcb32514e9ab2141e4727dbcb9677b7f168cb97
Supatz317 <supatsorn.su@kkumail.com>
2025-04-30 22:11:38 +0700

    view a summary of all expenses, view a summary of expenses for a specific month

---
+++ cmd/summary.go
@@ -0,0 +1,48 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"expense-tracker/model"
+
+	"github.com/spf13/cobra"
+)
+
+// summaryCmd represents the summary command
+var summaryCmd = &cobra.Command{
+	Use:   "summary",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		month, _ := cmd.Flags().GetInt("month")
+
+		if month == 0 {
+			// default is all
+			model.Exps.Summary()
+
+		} else {
+			model.Exps.SummaryByMonth(month)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(summaryCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// summaryCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// summaryCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
